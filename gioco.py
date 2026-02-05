c17fb5a78755f2b287b503e6ca9da823d11a3c63
y5ggggg <lorenzikkio10@gmail.com>
2026-02-05 11:52:53 +0100

    messo item

--- gioco.py
+++ gioco.py
@@ -5,15 +5,26 @@ WINDOW_HEIGHT = 720
 WINDOW_TITLE = "Platformer"
 
 TILE_SCALING = 0.5
+COIN_SCALING = 0.5
 PLAYER_MOVEMENT_SPEED = 5
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
+
 
 class mywindow(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+        self.player_texture = None
+        self.player_sprite = None
+        self.player_list = None
+
+        self.wall_list = None
+        self.camera = None
+        self.coin_list = None
+
+        
 
         self.lista_pilota = arcade.SpriteList()
         self.sfondo = arcade.load_texture("./immagini/sfondo1.png")
@@ -37,28 +48,43 @@ class mywindow(arcade.Window):
             wall.scale= 0.1211
             wall.position = coordinate
             self.wall_list.append(wall)
+        for x in range(128, 1250, 256):
+            coin = arcade.Sprite("./images/items.png", scale=COIN_SCALING)
+            coin.center_x = x
+            coin.center_y = 96
+            self.coin_list.append(coin)
         
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self.player_sprite, walls=self.wall_list, gravity_constant=GRAVITY
         )
-
+        self.camera = arcade.Camera2D()
         self.velocita = 4
 
         self.setup()
     
     def setup(self):
-        pass
+        self.coin_list = arcade.SpriteList(use_spatial_hash=True)
     
     def on_draw(self):
         self.clear()
+        self.camera.use()
+        self.coin_list.draw()
         arcade.draw.draw_texture_rect(self.sfondo, arcade.types.Viewport(0,0,WINDOW_WIDTH,WINDOW_HEIGHT))
         self.player_list.draw()
         self.wall_list.draw()
 
     def on_update(self, delta_time):
+        self.camera.position = self.player_sprite.position
         self.physics_engine.update()
-        
+        coin_hit_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.coin_list
+        )
+
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
     def on_key_press(self, tasto, modificatori):
+        if tasto == arcade.key.ESCAPE:
+            self.setup()
         if tasto == arcade.key.UP or tasto == arcade.key.W:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
