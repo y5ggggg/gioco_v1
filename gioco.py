fd8084b8b6f604e9af136025149e96846670bd34
y5ggggg <lorenzikkio10@gmail.com>
2026-02-05 10:15:12 +0100

    messo muro e terreno e sfondo

--- gioco.py
+++ gioco.py
@@ -4,6 +4,8 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "Platformer"
 
+TILE_SCALING = 0.5
+
 class mywindow(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
@@ -11,15 +13,30 @@ class mywindow(arcade.Window):
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
 
         self.lista_pilota = arcade.SpriteList()
-        #self.sfondo = arcade.load_texture("")
+        self.sfondo = arcade.load_texture("./immagini/sfondo1.png")
         self.jump_pressed = False
         self.left_pressed = False
         self.right_pressed = False
         self.pilota = arcade.load_texture("./immagini/pilota1.png")
         self.player_sprite = arcade.Sprite(self.pilota)
+        self.player_list=arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
         self.player_sprite.scale = 0.5
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("./immagini/tiles/terreno.png", scale=TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite("./immagini/tiles/muro.png", scale=TILE_SCALING)
+            wall.scale= 0.1211
+            wall.position = coordinate
+            self.wall_list.append(wall)
 
         self.velocita = 4
 
@@ -30,8 +47,9 @@ class mywindow(arcade.Window):
     
     def on_draw(self):
         self.clear()
-        #arcade.draw.draw_texture_rect(self.sfondo, arcade.types.Viewport(0,0,600,600))
-        arcade.draw_sprite(self.player_sprite)
+        arcade.draw.draw_texture_rect(self.sfondo, arcade.types.Viewport(0,0,WINDOW_WIDTH,WINDOW_HEIGHT))
+        self.player_list.draw()
+        self.wall_list.draw()
 
     def on_update(self, delta_time):
         # Calcola movimento in base ai tasti premuti
