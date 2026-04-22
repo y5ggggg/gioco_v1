0cd4172abc34d05d8ff63e22e727b10dac28af9f
y5ggggg <lorenzikkio10@gmail.com>
2026-04-22 17:50:59 +0200

    fixato movimento nemici

--- nemico.py
+++ nemico.py
@@ -15,15 +15,15 @@ class Nemico(arcade.Sprite):
         enemy.change_x = velocity
         enemy.patrol_origin = x
         enemy.velocità = velocity
-        self.stop_ = 0.0
+        enemy.stop_ = 0.0
         enemy.imposta_animazione("run")
         return enemy
     
     def on_update(self, delta_time: float):
     
         for enemy in self.enemy_list:
-            if self.stop_ >= 0:
-                self.stop_ -= delta_time
+            if enemy.stop_ >= 0:
+                enemy.stop_ -= delta_time
                 enemy.imposta_animazione("idle")
                 enemy.update_animation(delta_time)
                 continue
@@ -33,11 +33,11 @@ class Nemico(arcade.Sprite):
             distanza = enemy.center_x - enemy.patrol_origin
             if distanza <= -PATROL_RANGE:
                 enemy.center_x = enemy.patrol_origin - PATROL_RANGE
-                self.stop_=1.0
+                enemy.stop_=1.0
                 enemy.change_x = enemy.velocità
             elif distanza >= PATROL_RANGE:
                 enemy.center_x = enemy.patrol_origin + PATROL_RANGE
-                self.stop_=1.0  
+                enemy.stop_=1.0  
                 enemy.change_x = -enemy.velocità
 
             if enemy.change_x < 0: 
